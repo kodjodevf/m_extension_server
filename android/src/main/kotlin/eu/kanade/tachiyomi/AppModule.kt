afb3908993f0a249e6e597fbb74f719c9bdd0fcf
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2026-09-18 16:38:45 +0100

    feat: enhance JSON configuration in AppModule for improved serialization handling

--- android/src/main/kotlin/eu/kanade/tachiyomi/AppModule.kt
+++ android/src/main/kotlin/eu/kanade/tachiyomi/AppModule.kt
@@ -2,6 +2,7 @@ package eu.kanade.tachiyomi
 
 import android.app.Application
 import eu.kanade.tachiyomi.network.NetworkHelper
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import uy.kohesive.injekt.api.InjektModule
 import uy.kohesive.injekt.api.InjektRegistrar
@@ -11,11 +12,18 @@ import uy.kohesive.injekt.api.addSingletonFactory
 class AppModule(
     val app: Application,
 ) : InjektModule {
+    @OptIn(ExperimentalSerializationApi::class)
     override fun InjektRegistrar.registerInjectables() {
         addSingleton(app)
 
         addSingletonFactory { NetworkHelper(app) }
-        
-        addSingletonFactory { Json { ignoreUnknownKeys = true } }
+
+        addSingletonFactory {
+            Json {
+                ignoreUnknownKeys = true
+                explicitNulls = false
+                coerceInputValues = true
+            }
+        }
     }
 }
